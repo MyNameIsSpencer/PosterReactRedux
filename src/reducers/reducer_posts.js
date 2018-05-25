76b95157aeb11d9c7455c86831cbdc830922ff04
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-24 20:15:00 -0400

    add delete post functionality

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,21 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+  case DELETE_POST:
+    return _.omit(state, action.payload );
+    
+  case FETCH_POST:
+  // VVV is the ES5  way of doing things
+    // const post = action.payload.data;
+    // const newState = { ...state, };
+    // newState[post.id] = post;
+    // return newState;
+// Below ES6 return VVV  has same result as above ES5 ^^^
+    return {...state, [action.payload.data.id]: action.payload.data};
+
+
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data, 'id');
   default:
